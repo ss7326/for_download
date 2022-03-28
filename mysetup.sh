b43398cd44eff74d3341ed99b142bbc8f6b64f95
ss7326 <47410218+ss7326@users.noreply.github.com>
2022-03-29 01:43:39 +0900

    add install git

--- mysetup.sh
+++ mysetup.sh
@@ -40,3 +40,4 @@ apt-get install -y code
 
 sudo apt-get update
 sudo apt-get -y install \ bash \ coreutils \ grep \ iproute 2 \ iputils-ping \ traceroute \ tcpdump \ bind 9-dnsutils \ dnsmasq-base \ netcat-openbsd \ python 3 \ curl \ wget \ iptables \ procps \ isc-dhcp-client
+sudo apt install git
