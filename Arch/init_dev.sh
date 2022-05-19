fcb2b568d024d058cd7597d71327ab4b7e25a974
ss7326 <47410218+ss7326@users.noreply.github.com>
2022-05-19 22:00:19 +0900

    init

---
+++ Arch/init_dev.sh
@@ -0,0 +1,30 @@
+yay -S google-chrome
+yay -S visual-studio-code-bin
+
+yay -S git
+yay -S firefox
+yay -S virtualbox
+yay -S vlc
+yay -S ufw
+yay -S sysstat
+yay -S maim
+yay -S gthumb
+yay -S htop
+yay -S powertop
+yay -S mplayer
+yay -S xbindkeys
+yay -S dwb
+yay -S feh
+yay -S spacefm
+yay -S dropbox
+
+yay -S strings
+yay -S clipit
+yay -S xclip
+yay -S libreoffece
+
+yay -S bash
+yay -S vim
+yay -S emacs
+
+yay -S dolphin
