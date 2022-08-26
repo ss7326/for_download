269f92439c5e1f7ddce74c49211082a4562d4534
ss7326 <47410218+ss7326@users.noreply.github.com>
2022-08-27 01:09:19 +0900

    'add

--- ctf/ctf_setup_for_linux.sh
+++ ctf/ctf_setup_for_linux.sh
@@ -4,6 +4,8 @@ sudo apt-get install tmux
 sudo apt-get install strace -y
 sudo apt-get install ltrace -y
 
+sudo apt-get install python3
+
 sudo apt-get -y install \ bash \ coreutils \ grep \ iproute 2 \ iputils-ping \ traceroute \ tcpdump \ bind 9-dnsutils \ dnsmasq-base \ netcat-openbsd \ python 3 \ curl \ wget \ iptables \ procps \ isc-dhcp-client
 sudo apt -y install git
 sudo apt -y install gedit
