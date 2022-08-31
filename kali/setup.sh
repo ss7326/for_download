c50d26386e355b312fa1bbdc4a9ac4d191a3cff6
ss7326 <47410218+ss7326@users.noreply.github.com>
2022-09-01 04:17:21 +0900

    _

--- kali/setup.sh
+++ kali/setup.sh
@@ -8,3 +8,7 @@ sudo apt install gedit
 sudo apt install git
 sudo apt install vim
 sudo apt install gcc
+sudo apt install strace -y
+sudo apt install ltrace -y
+
+sudo apt-get -y install \ bash \ coreutils \ grep \ iproute 2 \ iputils-ping \ traceroute \ tcpdump \ bind 9-dnsutils \ dnsmasq-base \ netcat-openbsd \ python 3 \ curl \ wget \ iptables \ procps \ isc-dhcp-client
